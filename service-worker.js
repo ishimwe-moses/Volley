8428082dd7049742c6529a7649f76652fde0b7b5
ishimwe-moses <160502579+ishimwe-moses@users.noreply.github.com>
2024-11-01 15:54:40 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+</script>
+const cacheName = 'site-dynamic-cache-v1';
+
+// Install the service worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('Opened cache');
+      return cache.add('/');
+    })
+  );
+  self.skipWaiting(); // Activate the new service worker immediately
+});
+
+// Fetch files from cache or network, and cache them dynamically
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Serve the cache first
+      if (response) {
+        return response;
+      }
+
+      // If not cached, fetch from network and cache the result
+      return fetch(event.request).then((fetchResponse) => {
+        return caches.open(cacheName).then((cache) => {
+          cache.put(event.request, fetchResponse.clone());
+          return fetchResponse;
+        });
+      });
+    })
+  );
+});
+
+// Activate the new service worker and remove old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [cacheName];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (!cacheWhitelist.includes(cache)) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
